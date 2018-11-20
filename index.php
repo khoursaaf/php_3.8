dbebdba58dd5bf9c5189f7b25c3d5ddeeb62c79d
khoursaaf <khoursa@gmail.com>
2018-11-20 16:27:16 +0100

    correction

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>Exercise</title>
-</head>
-<body>
-  <p>En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.</p>
-  <?php
-  for ($a=200; $a >= 0 ; $a -= 12 ) {
-    echo $a."Enfin!!!";
-  }
-  ?>
-  <?php
-  $a = 200;
-  while ($a >= 0) {
-    echo $a."Enfin!!!";
-    $a -= 12;
-  }
-  ?>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.</p>
+    <p>Premier example</p>
+    <?php
+    for ($a = 200; $a >= 0 ; $a -= 12 ) { ?>
+    <p><?php echo $a."Enfin!!!"; } ?></p>
+    <p>Deuxieme exemple</p>
+    <?php
+     $a = 200;
+     while ($a >= 0) { ?>
+    <p><?php echo $a."Enfin!!!"; ?></p>
+    <?php $a -= 12; } ?>
+  </body>
 </html>
